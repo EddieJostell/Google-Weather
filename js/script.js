06467cd0c46aeedc44e25099b0995a3fe85a684a
Eddie Jostell <eddjos@saltsea.net>
2017-10-06 18:12:14 +0200

    map, markers, infowindow with weather is working

--- js/script.js
+++ js/script.js
@@ -1,10 +1,6 @@
 
 function initMap(){
-    
-    
     //AIzaSyCt7sLtWFV6-iUi00e1gbz43NnlrGn6jOo
-    
-    
     let location = {lat: 52.520007, lng: 13.404954};
     var map = new google.maps.Map(document.getElementById('map'), {
         styles: [{
@@ -14,11 +10,10 @@ function initMap(){
                 "visibility": "on"
             }]
         }],
-        zoom: 4,
+        zoom: 5,
         center: location
     });
 
-    
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) { 
@@ -27,13 +22,11 @@ function initMap(){
             for (var key in myObj) {
                 if (myObj.hasOwnProperty(key)) {
                     var element = myObj[key];
-                    //console.log(element.CapitalName);
                     createMarkers(
                     parseFloat(element.CapitalLatitude),
                     parseFloat(element.CapitalLongitude), 
                     map,  
                     element.CapitalName);
-
                 }
             }
         }
@@ -41,96 +34,93 @@ function initMap(){
     xmlhttp.open("GET", "country-capitals.json", true);
     xmlhttp.send();
     
-    
-    /* var myLatLng = {lat: 48.856614, lng: 2.352222};
-    var marker = new google.maps.Marker({
-        position: myLatLng,
-        map: map,
-        title: 'Paris'
-    }); */    
+   // var myLatLng = {lat: 48.856614, lng: 2.352222};
+   // var marker = new google.maps.Marker({
+     //   position: myLatLng,
+      //  map: map,
+      //  title: 'Paris'
+    //});    
+
 } 
 
-function createMarkers(lat, long, map, capital) {
+  function createMarkers(lat, long, map, capital) {
         let marker = new google.maps.Marker({ 
             position: {lat:lat, lng: long},
             map: map,
         });
-        
-        createInfoWindow(capital, marker, map, lat, long);        
-        //}
+        createInfoWindow(capital, marker, map, lat, long);  
     }
     
-    function createInfoWindow(capital, marker, map, lat, long, winfo) {
-        //console.log("YOLO", winfo);
+    var InfoWindows = [];
 
-       /*  let weatherInfo = `<div class="">
-        <div class="card-block">
-        <h6 class="card-title">Temp:  Degrees Celcius</h6>
-        <p class="card-text">Pressure</p>				
-        <p class="card-text">Humidity</p>
-        <p class="card-text">Clouds in the Sky:  </p>
-        <p class="card-text">Name: </p>
-        </div>
-        </div>`; */
-      
-    
+    function createInfoWindow(capital, marker, map, lat, long, weatherInfo) {
         let infowindow = new google.maps.InfoWindow({
-            content: winfo
+            //content: capital
         });
-
+        InfoWindows.push(infowindow);
         
+        function closeAllInfoWindows() {
+            for (var i = 0; i < InfoWindows.length; i++) {
+                    InfoWindows[i].close();
+                
+            }
+        }
+
         marker.addListener('click', function() {
+            console.log("On Marker click invoke weather function");
             //console.log("YOLO", lat, long)
-           getCurrentWeatherFromAPI(lat, long);
-           
+            getCurrentWeatherFromAPI(lat, long, infowindow);   
+            closeAllInfoWindows();        
             infowindow.open(map, marker);
         });
     }
     
     
-    function getCurrentWeatherFromAPI(lat, long) {
-        
-        //006595c752436e02740e9d8ff6b6cd05
-        let weather = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=metric&APPID=006595c752436e02740e9d8ff6b6cd05`;
-        fetch(weather) 
+    function getCurrentWeatherFromAPI(lat, long, infowindow) {
+        console.log("getCurrentWeatherFromAPI, start as you click a marker");
+        //006595c752436e02740e9d8ff6b6cd05 API KEY FOR Eduardo
+        //eb3bc19f92d9df047f452e1230df445c API KEY FOR wardo1987
+        //let weather = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=metric&APPID=006595c752436e02740e9d8ff6b6cd05`;
+        //let weather2 = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=metric&APPID=eb3bc19f92d9df047f452e1230df445c`;
+        let weather3 = "sweden.json";
+           fetch(weather3) 
         .then((response) => {
             //console.log(response.json());
             let currentWeather = response.json();
-            //console.log(currentWeather);
+            console.log(currentWeather);
                 return currentWeather;    
         })
         .then(function(currentWeather) {
-            //console.log(currentWeather);
-            createWeatherLiteral(currentWeather);
-            //console.log(currentWeather.main.temp);
+            createWeatherLiteral(currentWeather, infowindow);
         })
         .catch(function(error) {
             console.log('Request failed', error); 
         });		
     }
    
-      function createWeatherLiteral(info) {
-        let weatherInfo = `<div class="">
-        <div class="card-block">
-        <h6 class="card-title">Temp: ${info.main.temp} Degrees Celcius</h6>
-        <p class="card-text">Pressure ${info.main.pressure}}</p>				
-        <p class="card-text">Humidity ${info.main.humidity}</p>
-        <p class="card-text">Clouds in the Sky: ${info.clouds.all} </p>
-        <p class="card-text">Name: ${info.name} </p>
-        </div>
+      function createWeatherLiteral(info, infowindow) {
+          console.log("createWeatherLiteral start as getCurrentWeatherFromAPI start?");
+        let weatherInfo = `<div class="window">
+        <h5 class="para">City: ${info.name} </h5>
+        <h5 class="title">Temperature: ${info.main.temp} °C</h5>
+        <h5 class="para">Wind: ${info.wind.speed} m/s / ${info.wind.deg} degrees</h5>
+        <h5 class="para">Lowest temperature: ${info.main.temp_min} °C</h5>
+        <h5 class="para">Highest temperature: ${info.main.temp_max} °C</h5>					
+        <h5 class="para">Humidity ${info.main.humidity}%</h5>
+        <h5 class="para">${info.weather[0].description} in the sky</h5>
         </div>`;
-        //console.log(weatherInfo);
-        createInfoWindow(weatherInfo);
-        BOOM(weatherInfo);
-    } 
+        infowindow.setContent(weatherInfo);
+        console.log(weatherInfo);
+        
+       
+    }  
+ 
 
-    function BOOM(w) {
-        console.log(w);
-    }
+   
+    
+   
 
-    /* function get5dayForecastFromAPI() {
-
-    } */
+ 
 
 
     
